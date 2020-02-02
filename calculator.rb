3faf861437cef2fc9c10aca4177d56bebba1a172
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-02 08:28:49 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,22 +1,5 @@
 # Add your variables here
-first_number = 1
-
-def calculatorSum(first_number, second_number)
-  sum = first_number + second_number
-  puts sum
-end
-
-def calculatorQuotient(first_number, second_number)
-  quotient = first_number / second_number
-  puts quotient
-end
-
-def calculatorProdcut(first_number, second_number)
-  product = first_number * second_number
-  puts product
-end
-
-def calculatorDifference(first_number,second_number)
-  difference = first_number - second_number
-  puts difference
-end
+first_number = 6
+second_number = 2
+sum = first_number + second_number
+puts sum
