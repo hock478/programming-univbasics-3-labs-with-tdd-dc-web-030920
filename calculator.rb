06f94699f51dbc578d8fdac4b4ac0c586b36c877
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-02 08:30:33 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,8 @@
 # Add your variables here
 first_number = 6
 second_number = 2
+
 sum = first_number + second_number
-puts sum
 difference = first_number - second_number
 product = first_number * second_number
+quotient = first_number / second_number
