3e6d4cf304bea9daa3e10be7083bbb8fd0ab3225
Dawit Gizaw <gizawdawit123@gmail.com>
2020-02-02 08:17:06 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,17 @@
 # Add your variables here
 def calculatorSum(first_number, second_number)
   sum = first_number + second_number
-  puts
+  puts sum
 end
+
+def calculatorQuotient(first_number, second_number)
+  quotient = first_number / second_number
+  puts quotient
+end
+
+def calculatorProdcut(first_number, second_number)
+  product = first_number * second_number
+  puts product
+end
+
+def calculatorSubtract(first)
